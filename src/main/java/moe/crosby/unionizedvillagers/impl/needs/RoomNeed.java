4744e879687b5d614665716d9fb621456c348f33
Crosby <32882447+crosby-moe@users.noreply.github.com>
2026-01-20 09:59:05 -0500

    use a floodfill-like algorithm for RoomNeed

--- src/main/java/moe/crosby/unionizedvillagers/impl/needs/RoomNeed.java
+++ src/main/java/moe/crosby/unionizedvillagers/impl/needs/RoomNeed.java
@@ -1,26 +1,27 @@
 package moe.crosby.unionizedvillagers.impl.needs;
 
+import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
 import moe.crosby.unionizedvillagers.api.VillagerNeed;
-import net.minecraft.entity.ai.FuzzyTargeting;
-import net.minecraft.entity.ai.brain.MemoryModuleType;
-import net.minecraft.entity.ai.pathing.Path;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.GlobalPos;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.Optional;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Set;
 
 /**
  * Villager need that ensures a villager is not trapped
  */
 public class RoomNeed extends VillagerNeed {
-    private static final int DISTANCE_TARGET = 5;
-    private static final int SQUARE_DISTANCE_TARGET = DISTANCE_TARGET * DISTANCE_TARGET;
-    private static final int SEARCH_RADIUS = 32;
+    private static final double ONE_VOXEL = 1 / 16d;
+    private static final double EPSILON = 1e-5f;
+    private static final int AREA = 3 * 3;
 
     public RoomNeed(Identifier identifier, int priority) {
         super(identifier, priority);
@@ -28,78 +29,80 @@ public class RoomNeed extends VillagerNeed {
 
     @Override
     public boolean isMet(World world, VillagerEntity villagerEntity, PlayerEntity playerEntity) {
-        // if currently pathing, check the distance
-        @Nullable Path path = villagerEntity.getNavigation().getCurrentPath();
-        if (path != null && path.getEnd() != null) {
-            if (path.getEnd().getSquaredDistance(villagerEntity.getBlockPos()) > SQUARE_DISTANCE_TARGET) {
-                debug(villagerEntity, true, () -> "existing pathing target at " + path.getEnd().getBlockPos());
-                return true;
+        double height = villagerEntity.getHeight();
+        int bodyBlocks = MathHelper.ceil(height - 1);
+        double headSize = MathHelper.clamp(height - bodyBlocks + ONE_VOXEL, 0, 1);
+        double jumpHeight = 1.2d;
+        double walkHeight = 0.5d;
+
+        ShapeContext shapeContext = ShapeContext.of(villagerEntity);
+        Set<BlockPos> visited = new ObjectOpenHashSet<>();
+        Queue<BlockPos> queue = new ArrayDeque<>();
+        queue.add(villagerEntity.getBlockPos());
+        visited.add(villagerEntity.getBlockPos());
+        int count = 0;
+
+        while (!queue.isEmpty() && count < AREA) {
+            BlockPos footPos = queue.poll();
+
+            double footHeight = footHeight(world, shapeContext, footPos);
+
+            if (count > 0) {
+                // attempt to move down
+                if (footHeight < EPSILON && headHeight(world, shapeContext, footPos.up(2)) > headSize) {
+                    BlockPos newFootPos = footPos.down();
+                    if (world.getBlockState(footPos).getCollisionShape(world, newFootPos, shapeContext).isEmpty()) {
+                        footPos = newFootPos;
+                    }
+                // attempt to move up
+                } else if (footHeight > walkHeight && footHeight <= jumpHeight && headHeight(world, shapeContext, footPos.up(2)) > headSize) {
+                    BlockPos newFootPos = footPos.up();
+                    if (world.getBlockState(footPos).getCollisionShape(world, newFootPos, shapeContext).isEmpty()) {
+                        footPos = newFootPos;
+                    }
+                }
+            }
+
+            // check if the whole body fits
+            if (!canBodyFit(world, footPos, bodyBlocks, shapeContext)) {
+                continue;
+            }
+
+            // count block & increase queue
+            count++;
+            for (int i = 0; i < 4; i++) {
+                Direction direction = Direction.fromHorizontal(i);
+                BlockPos offsetPos = footPos.offset(direction);
+                if (visited.add(offsetPos)) {
+                    queue.add(offsetPos);
+                }
             }
         }
 
-        // get closest important location
-        Vec3d closestPoi = null;
+        boolean isMet = count >= AREA;
 
-        Optional<GlobalPos> homePoi = villagerEntity.getBrain().getOptionalRegisteredMemory(MemoryModuleType.HOME);
-        if (homePoi.isPresent() && homePoi.get().getDimension() == world.getRegistryKey()) {
-            Vec3d homePos = homePoi.get().getPos().toCenterPos();
-            if (villagerEntity.squaredDistanceTo(homePos) > SQUARE_DISTANCE_TARGET) {
-                debug(villagerEntity, true, () -> "accessible home at " + homePos);
-                return true;
-            }
-            closestPoi = homePos;
-        }
+        debug(villagerEntity, isMet, null);
 
-        Optional<GlobalPos> jobPoi = villagerEntity.getBrain().getOptionalRegisteredMemory(MemoryModuleType.JOB_SITE);
-        if (jobPoi.isPresent() && jobPoi.get().getDimension() == world.getRegistryKey()) {
-            Vec3d jobPos = jobPoi.get().getPos().toCenterPos();
-            if (villagerEntity.squaredDistanceTo(jobPos) > SQUARE_DISTANCE_TARGET) {
-                debug(villagerEntity, true, () -> "accessible job site at " + jobPos);
-                return true;
-            }
+        return isMet;
+    }
 
-            if (closestPoi == null || villagerEntity.squaredDistanceTo(jobPos) < villagerEntity.squaredDistanceTo(closestPoi)) {
-                closestPoi = jobPos;
+    private static double footHeight(World world, ShapeContext shapeContext, BlockPos pos) {
+        return world.getBlockState(pos).getCollisionShape(world, pos, shapeContext).getMax(Direction.Axis.Y);
+    }
+
+    private static double headHeight(World world, ShapeContext shapeContext, BlockPos pos) {
+        return world.getBlockState(pos).getCollisionShape(world, pos, shapeContext).getMin(Direction.Axis.Y);
+    }
+
+    private static boolean canBodyFit(World world, BlockPos footPos, int body, ShapeContext shapeContext) {
+        BlockPos headPos = footPos;
+        for (int i = 0; i < body; i++) {
+            headPos = headPos.up();
+            if (!world.getBlockState(headPos).getCollisionShape(world, headPos, shapeContext).isEmpty()) {
+                return false;
             }
         }
 
-        Optional<GlobalPos> potentialJobPoi = villagerEntity.getBrain().getOptionalRegisteredMemory(MemoryModuleType.POTENTIAL_JOB_SITE);
-        if (potentialJobPoi.isPresent() && potentialJobPoi.get().getDimension() == world.getRegistryKey()) {
-            Vec3d potentialJobPos = potentialJobPoi.get().getPos().toCenterPos();
-            if (villagerEntity.squaredDistanceTo(potentialJobPos) > SQUARE_DISTANCE_TARGET) {
-                debug(villagerEntity, true, () -> "accessible potential job site at " + potentialJobPos);
-                return true;
-            }
-
-            if (closestPoi == null || villagerEntity.squaredDistanceTo(potentialJobPos) < villagerEntity.squaredDistanceTo(closestPoi)) {
-                closestPoi = potentialJobPos;
-            }
-        }
-
-        Optional<GlobalPos> meetingPointPoi = villagerEntity.getBrain().getOptionalRegisteredMemory(MemoryModuleType.MEETING_POINT);
-        if (meetingPointPoi.isPresent() && meetingPointPoi.get().getDimension() == world.getRegistryKey()) {
-            Vec3d meetingPointPos = meetingPointPoi.get().getPos().toCenterPos();
-            if (villagerEntity.squaredDistanceTo(meetingPointPos) > SQUARE_DISTANCE_TARGET) {
-                debug(villagerEntity, true, () -> "accessible meeting point at " + meetingPointPos);
-                return true;
-            }
-
-            if (closestPoi == null || villagerEntity.squaredDistanceTo(meetingPointPos) < villagerEntity.squaredDistanceTo(closestPoi)) {
-                closestPoi = meetingPointPos;
-            }
-        }
-
-        // find reachable position further than target
-        for (int i = 0; i < 10; i++) {
-            Vec3d target = FuzzyTargeting.findFrom(villagerEntity, SEARCH_RADIUS, SEARCH_RADIUS / 2, closestPoi);
-            if (target != null && villagerEntity.squaredDistanceTo(target) > SQUARE_DISTANCE_TARGET) {
-                debug(villagerEntity, true, () -> "can path to " + target);
-                return true;
-            }
-        }
-
-        debug(villagerEntity, false, null);
-
-        return false;
+        return true;
     }
 }
